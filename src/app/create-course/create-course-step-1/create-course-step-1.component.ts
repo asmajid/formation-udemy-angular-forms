5af026a4b4b87880163bbec5eb2e7d8041aa4ad9
Asmaa Majid <majid.asmaa2001@gmail.com>
2023-08-09 13:57:06 +0100

    multi-step reactive form

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -10,6 +10,17 @@ import {filter} from 'rxjs/operators';
   styleUrls: ['./create-course-step-1.component.scss']
 })
 export class CreateCourseStep1Component implements OnInit {
+  form = this.fb.group({
+    title: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(60)
+    ]]
+
+  })
+  constructor(private fb: FormBuilder) {
+
+  }
 
   ngOnInit() {
 
